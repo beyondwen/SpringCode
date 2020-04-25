165bd2c75917c2a42584fcdb9306997038ab53a2
wenhao <921790476@qq.com>
2020-04-25 18:39:11 +0800

    @Qualifier @Primary 结合@Autowired 来指定一个接口下多实现的使用

--- src/main/java/org/wenhao/v9/V9Test.java
+++ src/main/java/org/wenhao/v9/V9Test.java
@@ -1,15 +1,18 @@
 package org.wenhao.v9;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.wenhao.v9.config.MyConfig;
-import org.wenhao.v9.service.impl.OrderServiceImpl;
 
 public class V9Test {
     static AnnotationConfigApplicationContext context;
 
     public static void main(String[] args) {
-        context = new AnnotationConfigApplicationContext(MyConfig.class);
+        /*context = new AnnotationConfigApplicationContext(MyConfig.class);
         OrderServiceImpl orderServiceImpl = (OrderServiceImpl) context.getBean("orderServiceImpl");
-        orderServiceImpl.add();
+        orderServiceImpl.add();*/
+        context = new AnnotationConfigApplicationContext();
+        String[] beanDefinitionNames = context.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println(beanDefinitionName);
+        }
     }
 }
